193f660dfe3aa111286b83298193d6f7a378a8cd
Maksim Ramanenkau <cbnhrpo@gmail.com>
2025-02-01 11:56:19 +0100

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationExceptionFilter } from './filters/validation-exception.filter
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   app.useGlobalFilters(new ValidationExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
